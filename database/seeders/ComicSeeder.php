42a127faa644798f74cf12e7147f93736b46544a
StefanDellamore <dellamoreste.info@gmail.com>
2024-02-29 18:16:41 +0100

    add seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Termwind\Components\Dd;
+use App\Models\Comic;
 
 class ComicSeeder extends Seeder
 {
@@ -12,6 +14,20 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $comicsData = config('comic');
+
+        foreach ($comicsData as $index => $singleComic) {
+            $comic = new Comic();
+            $comic->title = $singleComic ['title'];
+            $comic->description = $singleComic ['description'];
+            $comic->thumb = $singleComic ['thumb'];
+            $comic->price = $singleComic ['price'];
+            $comic->series = $singleComic ['series'];
+            $comic->sale_date = $singleComic ['sale_date'];
+            $comic->type = $singleComic ['type'];
+            //$comic->artists = $singleComic ['artists'];
+            //$comic->writers = $singleComic ['writers'];
+            $comic->save();
+        }
     }
 }
